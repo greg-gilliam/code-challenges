be4dc85d3e60f0347366302d0866fab03dd3db4a
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-10 11:14:28 -0700

    solved 1 and 2, stuck on illuminati

---
+++ src/codeChallenge01.test.js
@@ -0,0 +1,7 @@
+import { doubleNumbers } from './codeChallenge01.js';
+
+test('doubles numbers', () => {
+  const input = [2, 5, 100]; // arrange
+  const output = doubleNumbers(input); // act
+  expect(output).toEqual([4, 10, 200]); // assert
+});
