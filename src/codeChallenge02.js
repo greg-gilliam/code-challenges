aba6922a9f85c8b37befca3c24c1bd7381e1095a
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-11 18:12:41 -0700

    mostly done day2

--- src/codeChallenge02.js
+++ src/codeChallenge02.js
@@ -6,8 +6,26 @@ export function evensOnly(arr) {
     return arr.filter((num => num % 2 === 0));
 }
 
-// export function peopleWhoBelongToTheIlluminati(arr){
-//     peopleWhoBelongToTheIlluminati.keys(peopleWhoBelongToTheIlluminati)
-//         .filter((key) => peopleWhoBelongToTheIlluminati[member])
-//         .forEach((key) => true[member] =peopleWhoBelongToTheIlluminati[member]);
-// }
+export function fiveCharactersOrFewerOnly(arr) {
+    return arr.filter(string => string.length <= 5)
+}
+
+export function peopleWhoBelongToTheIlluminati(arr) {
+    return arr.filter((item) => {
+        return item.member === true;
+    })
+}
+
+export function ofAge(arr) {
+    return arr.filter((item) => {
+        return item.age > 18;
+    })
+}
+
+export function leastToGreatest(arr) {
+    return arr.sort((a, b) => a - b);
+}
+
+export function greatestToLeast(arr) {
+    return arr.sort((a, b) => b - a);
+}
