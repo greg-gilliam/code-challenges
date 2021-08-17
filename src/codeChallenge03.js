9e7e88a873952cbbb67368731a74103f4fd703f0
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-08-16 21:02:19 -0700

    two solved with working tests

--- src/codeChallenge03.js
+++ src/codeChallenge03.js
@@ -1,3 +1,11 @@
 export function sortedKeys(obj) {
     return Object.keys(obj).sort((a, b) => a.length - b.length);
 }
+
+// export function getFilteredKey(obj) {
+//     return Object.
+// }
+
+export function getArrayOfKeysAndValues(obj){
+    return Object.entries(obj);
+};
